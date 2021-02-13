1c4dc9767bb88bd92f7cd96dda7cb18c04b23d0a
Jibidus <jibidus@gmail.com>
2021-02-13 21:30:00 +0100

    :gear: generate gem from scaffold

---
+++ lib/model_validator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "model_validator/version"
+
+module ModelValidator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/model_validator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ModelValidator
+  VERSION = "0.1.0"
+end
