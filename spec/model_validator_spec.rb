d654c978971e49a156aa88035779dffbac916107
Jibidus <jibidus@gmail.com>
2021-02-17 18:17:41 +0100

    :gear: add missing tests

--- spec/model_validator_spec.rb
+++ spec/model_validator_spec.rb
@@ -7,6 +7,9 @@ class DummyModel < ActiveRecord::Base
   validates :value, presence: true
 end
 
+class NothingHandler # rubocop:disable Lint/EmptyClass
+end
+
 FactoryBot.define do
   factory :dummy_model do
     value { "my value" }
@@ -86,6 +89,34 @@ RSpec.describe ModelValidator do
           log_handler.on_new_class(DummyModel)
         end
         it { expect(Rails.logger).to have_received(:info).with("Checking 3 DummyModel…") }
+      end
+    end
+  end
+
+  describe ModelValidator::Validator do
+    describe "run" do
+      context "with custom handler which does not implement any method" do
+        let(:handler) { NothingHandler.new }
+        let(:validator) { ModelValidator::Validator.new(handlers: [handler]) }
+        it { expect { validator.run }.not_to raise_error }
+      end
+
+      context "with many custom handlers" do
+        let(:handlers) { [double("handler1"), double("handler2")] }
+        let(:validator) { ModelValidator::Validator.new(handlers: handlers) }
+        let!(:valid_model) { create :dummy_model }
+        let!(:invalid_model) { build(:dummy_model, value: nil) }
+
+        before { invalid_model.save!(validate: false) }
+        it "calls all handlers" do
+          handlers.each do |handler|
+            expect(handler).to receive(:on_new_class).with(DummyModel)
+            expect(handler).to receive(:on_violation).with(invalid_model)
+            expect(handler).to receive(:after_validation).with(valid_model)
+            expect(handler).to receive(:after_validation).with(invalid_model)
+          end
+          validator.run
+        end
       end
     end
   end
